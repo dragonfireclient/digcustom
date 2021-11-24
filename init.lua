3094ed985f165e1abd688e2caaa4e9cf4c41a054
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-11-24 17:31:53 +0100

    max_time: Remove superflowous default, improve documentation

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ async(function()
 		if minetest.settings:get_bool("digcustom") and minetest.localplayer then
 			local list = (minetest.settings:get("digcustom_nodes") or ""):split(",")
 			local node_pos = minetest.find_node_near(minetest.localplayer:get_pos(), 5, list, true)
-			local max_time = tonumber(minetest.settings:get("digcustom_max_time")) or -1
+			local max_time = tonumber(minetest.settings:get("digcustom_max_time"))
 
 			if node_pos then
 				await(diglib.dig_node(node_pos, max_time))
